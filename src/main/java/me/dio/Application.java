95ffeb9d12c204c5a736d61501fd8fd0e32191e6
mrlptrc <murilopatricio@gmail.com>
2023-08-25 04:24:45 -0300

    Implementação da camada de domínio

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,14 +1,13 @@
-package me.dio.santanderdevweek;
+package me.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderDevWeekApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDevWeekApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
-	
